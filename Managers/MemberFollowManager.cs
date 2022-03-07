fe001117f2378a4aedb38bf681f4ea3bad2419c9
holeou05fsh <holeou05fsh@gmail.com>
2022-03-07 21:27:15 +0800

    coding>MemberFollowEditor

--- Managers/MemberFollowManager.cs
+++ Managers/MemberFollowManager.cs
@@ -1,4 +1,5 @@
 ﻿using MKForum.Helpers;
+using MKForum.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -9,12 +10,12 @@ namespace MKForum.Managers
 {
     public class MemberFollowManager
     {
-        public List<MemberFollowManager> GetMemberFollows(string MemberID)
+        public List<MemberFollow> GetMemberFollows(string MemberID)
         {
             string connectionStr = ConfigHelper.GetConnectionString();
             string commandText =
                 @"
-                    SELECT * FROM
+                    SELECT * FROM MemberFollows
                     WHERE MemberID = @MemberID;
                 ";
 
@@ -24,20 +25,105 @@ namespace MKForum.Managers
                 {
                     using(SqlCommand command = new SqlCommand(commandText, connection))
                     {
+                        List<MemberFollow> Follows = new List<MemberFollow>();
                         connection.Open();
+
+                        command.Parameters.AddWithValue("@MemberID", MemberID);
                         SqlDataReader reader = command.ExecuteReader();
+
                         while(reader.Read())
                         {
-
-
+                            MemberFollow Follow = new MemberFollow()
+                            {
+                                MemberID = (Guid)reader["MemberID"],
+                                PostID = (Guid)reader["PostID"],
+                                FollowStatus = (bool)reader["FollowStatus"],
+                                ReadedDate = (DateTime)reader["ReadedDate"],
+                                Replied = (bool)reader["Replied"],
+                            };
+                            Follows.Add(Follow);
                         }
+                        return Follows;
                     }    
                 }
 
             }
             catch(Exception ex)
             {
+                Logger.WriteLog("MemberFollowManager.GetMemberFollows", ex);
+                throw;
+            }
+        }
 
+        public MemberFollow GetMemberFollow(string MemberID, string PostID)
+        {
+            string connectionStr = ConfigHelper.GetConnectionString();
+            string commandText =
+                @"
+                    SELECT * FROM MemberFollows
+                    WHERE MemberID = @MemberID AND PostID = @PostID;
+                ";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionStr))
+                {
+                    using (SqlCommand command = new SqlCommand(commandText, connection))
+                    {
+                        connection.Open();
+
+                        command.Parameters.AddWithValue("@MemberID", MemberID);
+                        command.Parameters.AddWithValue("@PostID", PostID);
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        reader.Read();
+
+                        MemberFollow Follow = new MemberFollow()
+                        {
+                            MemberID = (Guid)reader["MemberID"],
+                            PostID = (Guid)reader["PostID"],
+                            FollowStatus = (bool)reader["FollowStatus"],
+                            ReadedDate = (DateTime)reader["ReadedDate"],
+                            Replied = (bool)reader["Replied"],
+                        };
+                        return Follow;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog("MemberFollowManager.GetMemberFollows", ex);
+                throw;
+            }
+        }
+
+        public void Updatetrack(string MemberID, string PostID, int FollowStatus)
+        {
+            string connStr = ConfigHelper.GetConnectionString();
+            string commandText =
+                @"
+                    UPDATE MemberFollows
+                    SET FollowStatus = @FollowStatus
+                    WHERE MemberID = @MemberID AND PostID = @PostID;
+                ";
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(connStr))
+                {
+                    using(SqlCommand command = new SqlCommand(commandText, connection))
+                    {
+                        connection.Open();
+                        command.Parameters.AddWithValue("@FollowStatus", FollowStatus);
+                        command.Parameters.AddWithValue("@MemberID", MemberID);
+                        command.Parameters.AddWithValue("@PostID", PostID);
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLog("Updatetrack", ex);
+                throw;
             }
         }
     }
--- Models/MemberFollow.cs
+++ Models/MemberFollow.cs
@@ -7,10 +7,10 @@ namespace MKForum.Models
 {
     public class MemberFollow
     {
-        public int MemberID { get; set; }
-        public int PostID { get; set; }
+        public Guid MemberID { get; set; }
+        public Guid PostID { get; set; }
         public bool FollowStatus { get; set; }
-        public DateTime FollowDate { get; set; }
+        public DateTime ReadedDate { get; set; }
         public bool Replied { get; set; }
     }
 }
